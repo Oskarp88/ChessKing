a86e737b335017f54ca9422aa238b87942be92ee
oscarWilliam21 <oscarwilliam210890@gmail.com>
2025-05-19 11:10:19 -0500

    add router auth login

--- router/authRouter.js
+++ router/authRouter.js
@@ -1,59 +1,13 @@
 const express = require('express');
 const authRouter = express.Router();
-const { generateToken, verifyToken } = require('../config/jwt');
 const passport = require('passport');
-const User = require('../model/User');
-const { comparePassword } = require('../helpers/authHelpers');
+
+const { login } = require('../controller/authController');
 
 // const User = require('../model/User');
 
 // Ruta de login
-authRouter.post('/login', async (req, res) => {
-  const { email , password } = req.body;
-
-  try {
-
-    //validation
-    if(!email || !password){
-      return res.status(404).send({
-          succes: false,
-          message: 'Invalid email or password'
-      });
-  }
-    // Buscar al usuario en la base de datos por su nombre de usuario
-    const user = await User.findOne({ email });
-
-    if (!user) {
-      return res.status(401).json({ 
-        success: false,
-        message: 'Email is not registerd'
-       });
-    }
-
-    // Comparar la contraseña proporcionada con la almacenada en la base de datos
-    const isPasswordValid = await comparePassword(password, user.password);
-
-    if (!isPasswordValid) {
-      return res.status(401).json({
-        success: false,
-        message: 'Invalid Password'
-       });
-    }
-
-    // Si las credenciales son válidas, genera un token JWT y envíalo en la respuesta
-    const token = generateToken(user);
-    res.json({
-      success: true,
-      message: 'login successfully',
-      user,
-      token });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      success: false,
-      message: 'Error en el servidor' });
-  }
-});
+authRouter.post('/login', login);
 // Ruta protegida de ejemplo
 authRouter.get('/protected', (req, res) => {
   const token = req.headers.authorization;
